3fbf8831739c3f65c6b6f36133fad29762885011
akiffo <jackakif@gmail.com>
2022-06-04 19:21:35 +0600

    other page update v2

--- Jenkins-cicd/client/src/Fib.js
+++ Jenkins-cicd/client/src/Fib.js
@@ -61,7 +61,7 @@ class Fib extends Component {
             value={this.state.index}
             onChange={(event) => this.setState({ index: event.target.value })}
           />
-          <button>Submit</button>
+          <button>Submit v2</button>
         </form>
 
         <h3>Checked Numbers:</h3>
