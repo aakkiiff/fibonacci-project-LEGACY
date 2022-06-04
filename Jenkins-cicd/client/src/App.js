ced46064df72efbcf1dfbd8ec306e22800dec688
akiffo <jackakif@gmail.com>
2022-06-04 19:40:27 +0600

    other page update v3

--- Jenkins-cicd/client/src/App.js
+++ Jenkins-cicd/client/src/App.js
@@ -11,7 +11,7 @@ function App() {
       <div className="App">
         <header className="App-header">
           <Link to="/">Home</Link>
-          <Link to="/otherpage">Other Page version 2</Link>
+          <Link to="/otherpage">Other Page version 3</Link>
         </header>
         
         <div>
