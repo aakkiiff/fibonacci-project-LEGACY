3cb355196ad6a7ae01757dfdfcef0ed263eabc28
akiffo <jackakif@gmail.com>
2022-06-04 14:17:41 +0600

    done

---
+++ DOCKER_COMPOSE_COMPLETE/client/src/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import OtherPage from './OtherPage';
+import Fib from './Fib';
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+      <Link to="/">Home </Link>
+      <p>  </p>
+      <Link to="/otherpage"> page1</Link>
+      <p>  </p>
+        <div>
+          
+          <Route exact path="/" component={Fib} />
+          <Route path="/otherpage" component={OtherPage} />
+        </div>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
